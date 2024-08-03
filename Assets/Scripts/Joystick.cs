99b3e6ee17785e34dfa88356b4eebcecbf4389e6
Artur Dziadziula <artur.dziadziula@gmail.com>
2024-08-03 14:47:51 +0200

    implement joystick

---
+++ Assets/Scripts/Joystick.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+{
+	[SerializeField] private float _radius;
+	[SerializeField] private RectTransform _handle;
+
+	private bool _canDrag;
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		if (Vector2.Distance(eventData.position, transform.position) <= _radius)
+		{
+			_canDrag = true;
+			OnDrag(eventData);
+		}
+	}
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		if (!_canDrag) return;
+
+		Vector2 joystickCenter = transform.position;
+
+		if (Vector2.Distance(eventData.position, transform.position) <= _radius)
+			_handle.position = eventData.position;
+		else
+			_handle.position = joystickCenter + (eventData.position - joystickCenter).normalized * _radius;
+	}
+
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		_handle.position = transform.position;
+		_canDrag = false;
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireSphere(transform.position, _radius);
+	}
+}
