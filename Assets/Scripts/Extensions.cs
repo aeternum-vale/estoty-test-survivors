491c7b96111242da03a6173f3a76657a3724c8f2
Artur Dziadziula <artur.dziadziula@gmail.com>
2024-08-03 18:46:04 +0200

    implement basic enemy

--- Assets/Scripts/Extensions.cs
+++ Assets/Scripts/Extensions.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 
 public static class Extensions
 {
-	public static Vector3 To2D(this Vector2 vector)
+	public static Vector3 WithX(this Vector3 vector, float x)
 	{
-		return new Vector3(vector.x, vector.y, 0);
+		return new Vector3(x, vector.y, vector.z);
+	}
+
+	public static Vector3 WithY(this Vector3 vector, float y)
+	{
+		return new Vector3(vector.x, y, vector.z);
 	}
 
 	public static Vector3 WithZ(this Vector3 vector, float z)
